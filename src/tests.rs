4f6e728180c7b84e82e96434f2a736dcfeda4ba7
AbrarNitk <abrar.nitk@gmail.com>
2023-02-07 22:23:41 +0530

    refactored the test cases

--- src/tests.rs
+++ src/tests.rs
@@ -1,28 +1,28 @@
+fn current_dir() -> camino::Utf8PathBuf {
+    let current_dir = std::env::current_dir().unwrap();
+    camino::Utf8PathBuf::from_path_buf(current_dir).unwrap()
+}
+
+fn walkdir_util(root: &camino::Utf8Path) -> Vec<(camino::Utf8PathBuf, String)> {
+    walkdir::WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| {
+            if !e.as_ref().unwrap().file_type().is_dir() {
+                Some(e)
+            } else {
+                None
+            }
+        })
+        .map(|e| {
+            camino::Utf8PathBuf::from(e.as_ref().unwrap().path().as_os_str().to_str().unwrap())
+        })
+        .map(|x| (x.into(), "todo!".to_string()))
+        .collect::<Vec<(camino::Utf8PathBuf, String)>>()
+}
+
 #[test]
 fn test1() {
     // TODO: Test Contain so many unwraps
-
-    fn current_dir() -> camino::Utf8PathBuf {
-        let current_dir = std::env::current_dir().unwrap();
-        camino::Utf8PathBuf::from_path_buf(current_dir).unwrap()
-    }
-
-    fn walkdir_util(root: &camino::Utf8Path) -> Vec<(camino::Utf8PathBuf, String)> {
-        walkdir::WalkDir::new(root)
-            .into_iter()
-            .filter_map(|e| {
-                if !e.as_ref().unwrap().file_type().is_dir() {
-                    Some(e)
-                } else {
-                    None
-                }
-            })
-            .map(|e| {
-                camino::Utf8PathBuf::from(e.as_ref().unwrap().path().as_os_str().to_str().unwrap())
-            })
-            .map(|x| (x.into(), "todo!".to_string()))
-            .collect::<Vec<(camino::Utf8PathBuf, String)>>()
-    }
 
     let root = current_dir().join("tejar-tests/1");
     let files: _ = walkdir_util(root.as_path())
